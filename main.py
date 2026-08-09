ec93c201d12df5f0625ba5cf307f191752f01a7c
SaiSanjanaSainni <saisanjanachowdary@gmail.com>
2026-08-09 16:24:22 +0530

    Update

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from conversation_manager import ConversationManager
-
+from tools import get_current_time
+from tools import calculate_area
 chatbot = ConversationManager(
-    "You are a helpful AI assistant."
+    """You are a helpful AI assistant.
+
+Use tools only when necessary.
+For normal greetings such as hello, hi, good morning,
+respond normally without calling any tool.
+"""
 )
 
 while True:
@@ -13,3 +19,5 @@ while True:
     reply = chatbot.chat(user)
 
     print("AI:", reply)
+
+
