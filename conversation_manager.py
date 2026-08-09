f81f33eefe1ba954e43e0e333dfeaf7a802f2cac
SaiSanjanaSainni <saisanjanachowdary@gmail.com>
2026-08-09 16:23:40 +0530

    Update

--- conversation_manager.py
+++ conversation_manager.py
@@ -1,4 +1,7 @@
 from llm_client import client
+from tool_schema import TOOLS
+import json
+from tools import get_current_time, calculate_area
 class ConversationManager:
     def __init__(self,system_prompt):
         self.system_prompt=system_prompt
@@ -18,16 +21,41 @@ class ConversationManager:
     def get_response(self):
         response=client.chat.completions.create(
             model="llama-3.3-70b-versatile",
-            messages=self.messages
+            messages=self.messages,
+            tools=TOOLS
         )
-        reply=response.choices[0].message.content
+        reply=response.choices[0].message
+        if reply.tool_calls:
+            tool_name=reply.tool_calls[0].function.name
+            if tool_name=="get_current_time":
+                result=get_current_time()
+                self.messages.append(
+                    {
+                        "role": "assistant",
+                        "content": result
+                    }
+                )
+                return result
+            if tool_name=="calculate_area":
+                arguments=json.loads(reply.tool_calls[0].function.arguments)
+                length=arguments["length"]
+                width=arguments["width"]
+                result=calculate_area(length,width)
+                self.messages.append(
+                    {
+                        "role": "assistant",
+                        "content": str(result)
+                    }
+                )
+                return result
+        r=reply.content
         self.messages.append(
             {
-                "role": "assistant",
-                "content": reply
+                "role":"assistant",
+                "content":r
             }
         )
-        return reply
+        return r
     def chat(self,text):
         self.add_user_message(text)
         return self.get_response()
