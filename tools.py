654b6ed7fb9153825736bac563c93173218d2276
SaiSanjanaSainni <saisanjanachowdary@gmail.com>
2026-08-11 22:14:17 +0530

    Update

--- tools.py
+++ tools.py
@@ -4,3 +4,12 @@ def get_current_time():
     return datetime.now().strftime("%H:%M:%S")
 def calculate_area(x,y):
     return x*y
+def read_file(file_name):
+    
+        try:
+            with open(file_name,"r") as file:
+                content=file.read()
+                return content
+        except:
+             return "File not found"
+        
